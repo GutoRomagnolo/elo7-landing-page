735170bdb2fb25804264a27b0a208ff0dc4dd393
GutoRomagnolo <guromagnolo23@hotmail.com>
2023-01-14 02:27:43 -0300

    feat: adicionando lazy load com estrategia de precarregamento na landing page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { QuicklinkStrategy, QuicklinkModule } from 'ngx-quicklink';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
@@ -7,7 +8,11 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    QuicklinkModule,
+    RouterModule.forRoot(routes, {
+    preloadingStrategy: QuicklinkStrategy
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
